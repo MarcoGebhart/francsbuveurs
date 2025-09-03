8547cf68b93835e4eea3f4a3bc2c203ce6507f9b
Marco Gebhart <marceric.gebhart@bbox.fr>
2025-09-03 16:34:18 +0200

    reglage multer

--- back/app/schemas/event.js
+++ back/app/schemas/event.js
@@ -6,7 +6,7 @@ export const eventSchema = z.object({
     description : z.string().trim().nonempty(),
     date: z.coerce.date(),
     hour: z.string().regex(/^([01]\d|2[0-3]):([0-5]\d)(:[0-5]\d)?$/, "L'heure doit être au format HH:mm ou HH:mm:ss"),
-    img: z.string().trim().nonempty(),
+    img: z.string().trim().nonempty().optional(),
     slug: z.string().nonempty(),
     id_app_user: z.coerce.number().int()
 });
